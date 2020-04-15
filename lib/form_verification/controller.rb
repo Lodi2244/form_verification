c1be30ecfbed08157be1de05b742258e7bb18ae9
Lodi2244 <e.letellier.dev@gmail.com>
2020-04-15 09:28:35 +0200

    removes before_filter deprecated callback

--- lib/form_verification/controller.rb
+++ lib/form_verification/controller.rb
@@ -6,7 +6,7 @@ module FormVerification
 
     module ClassMethods
       def verify_form_token(object_name, options = {})
-        before_filter(options) do
+        before_action(options) do
           unless params[:_verification_token] == object_name.to_s
             raise FormVerification::VerificationTokenNotFound.new(
               'The verification token could not be found for the form. ' \
